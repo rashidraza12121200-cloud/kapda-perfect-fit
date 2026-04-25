c7c78d6af4d7e177845145c5ab73c7df37039921
rashidraza12121200-cloud <rashidraza12121200@gmail.com>
2026-04-25 18:49:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route path="/login" element={<Index />} />
             <Route path="/product/:id" element={<ProductPage />} />
             <Route path="/ai-recommend" element={<AIRecommend />} />
             <Route path="/tailors" element={<TailorFinder />} />
